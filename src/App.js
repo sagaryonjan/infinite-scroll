ca77e79c8180f17fcba22956a8274efe7042b52d
sagaryonjan <sagaryonjan015@gmail.com>
2021-03-30 20:53:52 +0545

    React infinate scroll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './App.css';
+import { useEffect, useState } from 'react';
+import {getPostList, getSecondPostList} from './postList';
 
-function App() {
+const App = () => {
+
+  const style = {
+    height: 250,
+    border: "1px solid green",
+    margin: 6,
+    padding: 8
+  };
+  
+  const [posts, setPosts] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [counter, setCounter] = useState(1);
+
+
+  useEffect(() => {
+        setPosts(getPostList());
+  }, []);
+
+  const fetchData = () => {
+    setCounter(counter + 1);
+    if(counter  == 2){
+      setHasMore(false);
+      return;
+    }
+
+    setTimeout(() => {
+      setPosts([...posts,...getSecondPostList()]);
+    }, 4000);
+  };
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <InfiniteScroll
+        dataLength={posts?.length} //This is important field to render the next data
+        next={fetchData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        {posts &&
+          posts.map((post, index) => (
+            <div style = {style}>
+              <h4>{post.title}</h4>
+              <p>{post.description}</p>
+            </div>
+          ))}
+      </InfiniteScroll>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/postList.js
@@ -0,0 +1,54 @@
+export const getPostList = () =>{
+    return (
+       [
+        {
+           id: 1,
+           title: 'apple',
+           description : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+       },
+
+       {
+        id: 2,
+        title: 'bag',
+        description : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+       },
+
+       {
+        id: 3,
+        title: 'cat',
+        description : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+       },
+
+           
+       ]
+    )
+
+}
+
+export const getSecondPostList = () =>{
+    return (
+       [
+        {
+           id: 1,
+           title: 'dog',
+           description : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+       },
+
+       {
+        id: 2,
+        title: 'egg',
+        description : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+       },
+
+       {
+        id: 3,
+        title: 'fan',
+        description : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+       },
+
+           
+       ]
+    )
+
+}
+
